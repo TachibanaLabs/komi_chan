83cbca1602cf7c3886842e6707e7177c094f1100
Neftaly Luis <abi@airmail.cc>
2019-04-20 18:50:07 -0500

    La cague jaja salu2

--- config/config.exs
+++ config/config.exs
@@ -24,6 +24,8 @@ config :phoenix, :json_library, Jason
 
 config :mnesia, dir: '.mnesia/#{Mix.env()}/#{node()}'
 
+config :husky, json_codec: Jason
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
